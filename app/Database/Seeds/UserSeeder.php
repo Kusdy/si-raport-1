043b400469989b061d2f0fd685c28699ee8016dd
Moch Syarif Hidayat <62657189+mosyahi@users.noreply.github.com>
2023-11-29 21:33:55 +0700

    Update UserSeeder.php

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -15,30 +15,8 @@ class UserSeeder extends Seeder
                 'password' => password_hash('admin', PASSWORD_DEFAULT),
                 'role' => 'Admin'
             ],
-            [
-                'email' => 'guru@gmail.com',
-                'nama' => 'Moch Syarif Hidayat, S.T',
-                'password' => password_hash('guru', PASSWORD_DEFAULT),
-                'role' => 'Guru'
-            ],
-            [
-                'email' => 'siswa@gmail.com',
-                'nama' => 'Ahmad Rizki, S.T',
-                'password' => password_hash('siswa', PASSWORD_DEFAULT),
-                'role' => 'Siswa'
-            ],
-            [
-                'email' => 'wakel@gmail.com',
-                'nama' => 'Dimas Pangestu, S.T',
-                'password' => password_hash('wakel', PASSWORD_DEFAULT),
-                'role' => 'Wali kelas'
-            ],
-            [
-                'email' => 'kepsek@gmail.com',
-                'nama' => 'Mbuh Keder',
-                'password' => password_hash('kepsek', PASSWORD_DEFAULT),
-                'role' => 'Kepala sekolah'
-            ],
+
+            // Role selain admin, silahkan bikin akun sendiri (generate yaa)
         ];
 
         $this->db->table('tb_user')->insertBatch($data);
