be5dcc338c3a905a4b3fda0e73a93bf989ccd6e9
Moch Syarif Hidayat <62657189+mosyahi@users.noreply.github.com>
2023-11-29 21:40:27 +0700

    Foot

--- app/Views/layouts/dashboard/footer.php
+++ app/Views/layouts/dashboard/footer.php
@@ -2,7 +2,7 @@
 	<footer class="bg-light">
 		<div class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 container-p-x py-3">
 			<div class="text-center text-md-start">
-				&copy; Template by <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/landing/" target="_blank" class="footer-text fw-bolder">Sneat</a> &nbsp; | &nbsp; &copy; 2023 Apps by <a href="https://www.instagram.com/mochsyarifhidayat13" target="_blank" class="footer-text fw-bolder">Mosyahizuku.</a>
+				&copy; Template by <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/landing/" target="_blank" class="footer-text fw-bolder">Sneat</a> &nbsp; | &nbsp; &copy; 2023 Apps by <a href="https://www.instagram.com/web.crafser" target="_blank" class="footer-text fw-bolder">Web Crafser.</a>
 			</div>
 		</div>
 	</footer>
